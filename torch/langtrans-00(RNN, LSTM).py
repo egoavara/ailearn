97e1a826b638ed859d149eb6d5f242d2c1a79916
kane.woz(김성현)/kakao <kane.woz@kakaocorp.com>
2024-05-21 18:53:14 +0900

    feat: mps 지원 추가

--- torch/langtrans-00(RNN, LSTM).py
+++ torch/langtrans-00(RNN, LSTM).py
@@ -18,7 +18,7 @@ from tqdm import tqdm
 MODEL_PATH = "langtrans-00(RNN, LSTM).pt"
 
 # Check if CUDA is available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
 print(f"Using device: {device}")
 
 # %%
